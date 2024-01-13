bdc2b365d6a8147e949b7546908d494226bb1bd8
RookieAND <gwangin1999@naver.com>
2024-01-13 19:11:07 +0900

    fix: 불필요한 Default Import Module 수정
    - React, ReactDOM 모듈 import 구조를 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import { StrictMode } from 'react';
 
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
         <App />
-    </React.StrictMode>,
+    </StrictMode>,
 );
