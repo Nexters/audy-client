6fe3ab6f2823eb4f11fa6c39f5f5f21b7d391548
KimGaeun <greencloud11@hanyang.ac.kr>
2024-01-18 01:01:21 +0900

    ✨ 애플로그인 기능 추가
    - 잘못 사용된 env 변수 수정
    - kakaoLoginLink 쿼리들 분리
    - 애플로그인 링크 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,43 @@
 export default function LoginPage() {
-    const kakaoLoginLink = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECTION_URL}&response_type=code`;
-
-    const handleKakaoLogin = () => {
-        window.location.href = kakaoLoginLink;
+    const appleLoginConfig = {
+        client_id: import.meta.env.VITE_APPLE_LOGIN_CLIENT_ID as string,
+        redirect_uri: import.meta.env.VITE_APPLE_LOGIN_REDIRECT_URL as string,
+        response_type: 'code id_token',
+        state: 'origin:web',
+        scope: 'name email',
+        response_mode: 'form_post',
+        m: 11,
+        v: '1.5.4',
+        // TODO: 임시 속성들. 추후 논의 후 수정해야 함
     };
+
+    const kakaoLoginConfig = {
+        client_id: import.meta.env.VITE_KAKAO_REST_API_KEY as string,
+        redirect_uri: import.meta.env.VITE_KAKAO_REDIRECTION_URL as string,
+        response_type: 'code',
+    };
+
+    const makeQueryString = (
+        config: Record<string, string | number | boolean>,
+    ) => {
+        return Object.entries(config)
+            .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+            .join('&');
+    };
+
+    const kakaoLoginLink = `https://kauth.kakao.com/oauth/authorize?${makeQueryString(kakaoLoginConfig)}`;
+    const appleLoginLink = `https://appleid.apple.com/auth/authorize?${makeQueryString(appleLoginConfig)}`;
+
+    const handleKakaoLogin = () => (window.location.href = kakaoLoginLink);
+    const handleAppleLogin = () => (window.location.href = appleLoginLink);
 
     return (
         <div>
             <button type="button" onClick={handleKakaoLogin}>
                 카카오로 로그인
+            </button>
+            <button type="button" onClick={handleAppleLogin}>
+                애플로 로그인
             </button>
         </div>
     );
