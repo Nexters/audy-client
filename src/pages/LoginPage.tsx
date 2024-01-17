cd519a5689a37baffe912d386d6b2bcdc15ee904
KimGaeun <greencloud11@hanyang.ac.kr>
2024-01-18 00:27:32 +0900

    ♻️ API_KEY와 REDIRECT_URL을 .env로 이동

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,13 @@
 export default function LoginPage() {
-    const KAKAO_REST_API_KEY = ''; // TODO: 임시 KEY
-    const KAKAO_REDIRECT_URI = 'http://localhost:5173/redirect'; // TODO: 임시 URL
-    const kakaoLoginLink = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+    const kakaoLoginLink = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECTION_URL}&response_type=code`;
 
-    const handleLogin = () => (window.location.href = kakaoLoginLink);
+    const handleKakaoLogin = () => {
+        window.location.href = kakaoLoginLink;
+    };
+
     return (
         <div>
-            <button type="button" onClick={handleLogin}>
+            <button type="button" onClick={handleKakaoLogin}>
                 카카오로 로그인
             </button>
         </div>
