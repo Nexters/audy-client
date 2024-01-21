328d30efeff974c784ff38c405a1ad967ccdd6e7
Gwangin Baik <gwangin1999@naver.com>
2024-01-21 22:22:52 +0900

    [AUD-17] API 공용 모듈 제작 및 React Query 세팅 (#7)
    * :memo: .env.template 에 SERVER_URL 추가
    
    * :sparkles: Axios 기반의 API 공용 유틸 모듈 추가
    
    * ✨ React Query 기초 세팅 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(<App />);
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
+createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+    </QueryClientProvider>,
+);
