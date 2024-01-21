63f24ae845eea642cd4879e349ccd190d04d6583
Baik Gwangin <gwangin1999@naver.com>
2024-01-21 22:41:14 +0900

    :fire: App.tsx 삭제 및 MainPage.tsx 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import App from "@/App";
+import MainPage from "@/pages/MainPage";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         // FIXME : 공통 fallback 컴포넌트 적용 필요
         errorElement: <div>에러</div>,
-        element: <App />,
+        element: <MainPage />,
         children: [
             {
                 path: "auth",
-                element: <App />
+                element: <MainPage />
             }
         ]
     },
