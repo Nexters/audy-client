e7d3fc8360d714ea95d368594d7460a6c18ffebf
KimGaeun <greencloud11@hanyang.ac.kr>
2024-01-21 21:49:59 +0900

    🎨 LoginPage의 상수를 constants 폴더로 이동

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,30 +1,7 @@
+import { socialLoginProvider } from '@/constants';
+
 export default function LoginPage() {
     type SocialLoginProviderType = 'apple' | 'kakao';
-
-    const socialLoginProvider = {
-        apple: {
-            url: 'https://appleid.apple.com/auth/authorize',
-            config: {
-                client_id: import.meta.env.VITE_APPLE_CLIENT_ID,
-                redirect_uri: import.meta.env.VITE_APPLE_REDIRECT_URL,
-                response_type: 'code id_token',
-                state: 'origin:web',
-                scope: 'name email',
-                response_mode: 'form_post',
-                m: 11,
-                v: '1.5.4',
-                // TODO: 임시 속성들. 추후 논의 후 수정해야 함
-            },
-        },
-        kakao: {
-            url: 'https://kauth.kakao.com/oauth/authorize',
-            config: {
-                client_id: import.meta.env.VITE_KAKAO_CLIENT_ID,
-                redirect_uri: import.meta.env.VITE_KAKAO_REDIRECT_URL,
-                response_type: 'code',
-            },
-        },
-    };
 
     const makeQueryString = (
         config: Record<string, string | number | boolean>,
