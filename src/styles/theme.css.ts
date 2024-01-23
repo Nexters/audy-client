62d3c68bc826f6505d13f359861a4ecf62598c6f
Baik Gwangin <gwangin1999@naver.com>
2024-01-24 00:40:20 +0900

    ✨ Color 시멘틱 토큰 추가 및 적용

--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -1,7 +1,8 @@
 import { createGlobalTheme } from '@vanilla-extract/css';
 
-import { FONT } from './foundation';
+import { FONT, COLOR } from './foundation';
 
 export const theme = createGlobalTheme(':root', {
     font: FONT,
+    color: COLOR,
 });
