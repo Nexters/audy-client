5792bc8af1e78fcdbda5ef2c61c208f7e52f65c6
RookieAND <gwangin1999@naver.com>
2024-01-23 20:46:07 +0900

    🐛 실수로 누락시켰던 코드를 원상 복구

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,35 +2,15 @@ import { socialLoginProvider } from '@/constants';
 import { SocialLoginProviderType } from '@/types';
 
 export default function LoginPage() {
-    type SocialLoginProvider = 'apple' | 'kakao';
-
-    const socialLoginProvider = {
-        apple: {
-            url: 'https://appleid.apple.com/auth/authorize',
-            config: {
-                client_id: import.meta.env.VITE_APPLE_CLIENT_ID,
-                redirect_uri: import.meta.env.VITE_APPLE_REDIRECT_URL,
-                response_type: 'code id_token',
-                state: 'origin:web',
-                scope: 'name email',
-                response_mode: 'form_post',
-                m: 11,
-                v: '1.5.4',
-                // TODO: 임시 속성들. 추후 논의 후 수정해야 함
-            },
-        },
-        kakao: {
-            url: 'https://kauth.kakao.com/oauth/authorize',
-            config: {
-                client_id: import.meta.env.VITE_KAKAO_CLIENT_ID,
-                redirect_uri: import.meta.env.VITE_KAKAO_REDIRECT_URL,
-                response_type: 'code',
-            },
-        },
+    const makeQueryString = (
+        config: Record<string, string | number | boolean>,
+    ) => {
+        return Object.entries(config)
+            .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+            .join('&');
     };
-
+    
     const makeSocialLoginUrl = (provider: SocialLoginProviderType) => {
-
         const { url, config } = socialLoginProvider[provider];
         const queryString = makeQueryString(config);
 
