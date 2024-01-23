f824e32e7966c533cf2bcd2ec9dce53faecc86cc
KimGaeun <greencloud11@hanyang.ac.kr>
2024-01-23 16:37:03 +0900

    ♻️ searchRepository.getPOI 함수 리팩토링

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -1,26 +1,23 @@
 import { GetPOIType } from '@/types';
-import { makeQueryString } from '@/utils/queryString';
 
 import { getAsync } from './api';
 
 export const searchRepository = {
     getPOI: async (keyword: string) => {
-        const url = `https://apis.openapi.sk.com/tmap/pois`;
-        const params = {
-            version: 1,
-            format: 'json',
-            callback: 'result',
-            searchKeyword: keyword,
-            resCoordType: 'WGS84GEO',
-            reqCoordType: 'WGS84GEO',
-            count: 10,
-            searchType: 'all',
-            appKey: import.meta.env.VITE_TMAP_APP_KEY,
-        };
-
-        const response = await getAsync<GetPOIType>(
-            `${url}?${makeQueryString(params)}`,
-        );
+        const response = await getAsync<GetPOIType>('/pois', {
+            baseURL: `https://apis.openapi.sk.com/tmap`,
+            headers: { appKey: import.meta.env.VITE_TMAP_APP_KEY },
+            params: {
+                version: 1,
+                format: 'json',
+                callback: 'result',
+                searchKeyword: keyword,
+                resCoordType: 'WGS84GEO',
+                reqCoordType: 'WGS84GEO',
+                count: 10,
+                searchType: 'all',
+            },
+        });
 
         return response.searchPoiInfo.pois.poi;
     },
