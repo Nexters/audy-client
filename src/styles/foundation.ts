d34e894912bf3c3c12d298c72d213eb1db445955
Baik Gwangin <gwangin1999@naver.com>
2024-01-24 00:28:59 +0900

    ✨ typography 디자인 토큰 추가

--- src/styles/foundation.ts
+++ src/styles/foundation.ts
@@ -1,4 +1,59 @@
 export const FONT = {
-    pretendard:
-      'Pretendard',
-  };
+    pretendard: 'Pretendard',
+};
+
+const FONT_WEIGHT = {
+    regular: 300,
+    medium: 400,
+    semiBold: 500,
+    bold: 600,
+    extraBold: 700,
+    black: 800,
+};
+
+const LINE_HEIGHT = '140%';
+
+const FONT_SIZE = [...Array(21).keys()].map((num) => num + 10);
+
+export const TYPOGRAPHY = FONT_SIZE.reduce(
+    (previous, size) => ({
+        ...previous,
+        [`Regular${size}`]: {
+            fontSize: `${size}px`,
+            fontWeight: FONT_WEIGHT.regular,
+            lineHeight: LINE_HEIGHT,
+            letterSpacing: '0.03px',
+        },
+        [`Medium${size}`]: {
+            fontSize: `${size}px`,
+            fontWeight: FONT_WEIGHT.medium,
+            lineHeight: LINE_HEIGHT,
+            letterSpacing: '0.03px',
+        },
+        [`SemiBold${size}`]: {
+            fontSize: `${size}px`,
+            fontWeight: FONT_WEIGHT.semiBold,
+            lineHeight: LINE_HEIGHT,
+            letterSpacing: '0.03px',
+        },
+        [`Bold${size}`]: {
+            fontSize: `${size}px`,
+            fontWeight: FONT_WEIGHT.bold,
+            lineHeight: LINE_HEIGHT,
+            letterSpacing: '0.03px',
+        },
+        [`ExtraBold${size}`]: {
+            fontSize: `${size}px`,
+            fontWeight: FONT_WEIGHT.extraBold,
+            lineHeight: LINE_HEIGHT,
+            letterSpacing: '0.03px',
+        },
+        [`Black${size}`]: {
+            fontSize: `${size}px`,
+            fontWeight: FONT_WEIGHT.black,
+            lineHeight: LINE_HEIGHT,
+            letterSpacing: '0.03px',
+        },
+    }),
+    {},
+);
