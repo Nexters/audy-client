1d05f30e678a76b4a565079b6acb383ab1a7c846
KimGaeun <greencloud11@hanyang.ac.kr>
2024-01-23 16:37:35 +0900

    🔥 utils/queryString.ts 파일 삭제

--- src/utils/queryString.ts
+++
@@ -1,7 +0,0 @@
-export const makeQueryString = (
-    config: Record<string, string | number | boolean>,
-) => {
-    return Object.entries(config)
-        .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-        .join('&');
-};
