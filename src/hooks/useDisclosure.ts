c1ca3a3b440ad5b248ecdc69519997d5eb9d9406
Baik Gwangin <gwangin1999@naver.com>
2024-01-27 01:23:32 +0900

    ✨ useDisclosure 커스텀 훅 제작

---
+++ src/hooks/useDisclosure.ts
@@ -0,0 +1,18 @@
+import { useMemo, useState } from 'react';
+
+/**
+ * Boolean State 를 편리하게 처리하기 위한 Hook useDisclosure
+ */
+export const useDisclosure = (initialState: boolean = false) => {
+    const [value, setValue] = useState(initialState);
+
+    return useMemo(
+        () => ({
+            value,
+            open: () => setValue(true),
+            close: () => setValue(false),
+            toggle: () => setValue((prevState) => !prevState),
+        }),
+        [value],
+    );
+};
