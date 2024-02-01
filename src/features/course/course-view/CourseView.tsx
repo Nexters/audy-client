db27091930daf0078fb89e0c2691c16af483702b
Baik Gwangin <gwangin1999@naver.com>
2024-02-01 22:34:34 +0900

    ✨ DnD 로직에 debounce 적용

--- src/features/course/course-view/CourseView.tsx
+++ src/features/course/course-view/CourseView.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Reorder } from 'framer-motion';
 
 import CourseItem from '@/features/course/course-item';
+import { useDebounce } from '@/hooks/useDebounce';
 import { useEventListeners } from '@/hooks/useEventListeners';
 import { useTmap } from '@/hooks/useTmap';
 import type { MarkersType } from '@/types/map';
@@ -10,18 +11,30 @@ import type { MarkersType } from '@/types/map';
 import * as S from './CourseView.css';
 import CourseViewContextProvider from './CourseViewContextProvider';
 
+const REORDER_DELAY = 660;
+
 const CourseView = () => {
     const [markers, setMarkers] = useState<MarkersType[]>([]);
     const { tmapModuleRef } = useTmap();
 
-    useEventListeners('modifyMarkers', (event) => {
-        setMarkers([...event.detail]);
+    const { debounce } = useDebounce();
+
+    useEventListeners('createMarker', (event) => {
+        setMarkers((previous) => [...previous, event.detail]);
     });
 
-    const handleReorderMarker = (newOrder: MarkersType[]) => {
+    useEventListeners('removeMarker', (event) => {
+        setMarkers(markers.filter((_, index) => index !== event.detail));
+    });
+
+    const debouncedModifyMarker = debounce((newOrder: MarkersType[]) => {
         if (!tmapModuleRef.current) return;
         tmapModuleRef.current.modifyMarker(newOrder);
+    }, REORDER_DELAY);
+
+    const handleReorderMarker = (newOrder: MarkersType[]) => {
         setMarkers(newOrder);
+        debouncedModifyMarker(newOrder);
     };
 
     return (
