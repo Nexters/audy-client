aa736c518e769a24dda4cbf55fa916f01279d773
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-02 08:05:53 +0900

    ✨ 검색 기능 추가

--- src/features/search/search-bar/SearchBar.tsx
+++ src/features/search/search-bar/SearchBar.tsx
@@ -1,8 +1,40 @@
+import { useState } from 'react';
+
+import { TmapRepository } from '@/apis/tmap';
 import SearchIcon from '@/assets/icons/search.svg?react';
+import { SearchResultsType } from '@/types/search';
 
 import * as S from './SearchBar.css';
 
-const SearchBar = () => {
+interface PropsType {
+    setIsSearchMode: React.Dispatch<React.SetStateAction<boolean>>;
+    setSearchResults: React.Dispatch<React.SetStateAction<SearchResultsType>>;
+}
+
+const SearchBar = ({ setIsSearchMode, setSearchResults }: PropsType) => {
+    const [searchInputValue, setSearchInputValue] = useState('');
+
+    const handleSearch = async () => {
+        const response = await TmapRepository.getPoiSearch({
+            keyword: searchInputValue,
+        });
+
+        setSearchResults(response);
+    };
+
+    const handleSearchInput = ({
+        target,
+    }: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInputValue(target.value);
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            setIsSearchMode(true);
+            handleSearch();
+        }
+    };
+
     return (
         <div className={S.searchBox}>
             <div className={S.searchInner}>
@@ -10,6 +42,9 @@ const SearchBar = () => {
                 <input
                     className={S.searchInput}
                     placeholder="장소를 입력해주세요."
+                    onKeyDown={handleKeyDown}
+                    value={searchInputValue}
+                    onChange={handleSearchInput}
                 />
             </div>
         </div>
