aef2a6b51d48d944e692f454c5043aa6a3a5cd86
Baik Gwangin <gwangin1999@naver.com>
2024-02-04 01:36:12 +0900

    ✨ 로그인 페이지 기능 연동 1차 진행

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,38 +1,49 @@
-import { socialLoginProvider } from '@/constants';
-import { SocialLoginProviderType } from '@/types';
+import AppleLogoIcon from '@/assets/icons/appleLogo.svg.svg?react';
+import AudyLogoIcon from '@/assets/icons/audyLogo.svg?react';
+import KakaoLogoIcon from '@/assets/icons/kakaoLogo.svg.svg?react';
+import { SOCIAL_LOGIN_METADATA } from '@/constants';
+import { SocialPlatformType } from '@/types';
+
+import * as S from './LoginPage.css';
 
 const LoginPage = () => {
-    const makeQueryString = (
-        config: Record<string, string | number | boolean>,
-    ) => {
-        return Object.entries(config)
-            .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-            .join('&');
-    };
+    const handleLoginButtonClick = (socialType: SocialPlatformType) => {
+        const { url, config } = SOCIAL_LOGIN_METADATA[socialType];
 
-    const makeSocialLoginUrl = (provider: SocialLoginProviderType) => {
-        const { url, config } = socialLoginProvider[provider];
-        const queryString = makeQueryString(config);
+        const redirectUrl = new URL(url);
+        Object.entries(config).map(([queryKey, value]) =>
+            redirectUrl.searchParams.set(queryKey, value),
+        );
 
-        return `${url}?${queryString}`;
-    };
-
-    const handleKakaoLogin = () => {
-        window.location.href = makeSocialLoginUrl('kakao');
-    };
-
-    const handleAppleLogin = () => {
-        window.location.href = makeSocialLoginUrl('apple');
+        window.location.href = redirectUrl.toString();
     };
 
     return (
-        <div>
-            <button type="button" onClick={handleKakaoLogin}>
-                카카오로 로그인
-            </button>
-            <button type="button" onClick={handleAppleLogin}>
-                애플로 로그인
-            </button>
+        <div className={S.wrapper}>
+            <section className={S.loginContainer}>
+                <div className={S.introduceBox}>
+                    <AudyLogoIcon className={S.audyLogo} />
+                    <h5 className={S.introduceText}>함께 만드는 여행 지도</h5>
+                    <p className={S.loginNoticeText}>로그인 혹은 회원가입</p>
+                </div>
+                <div className={S.buttonBox}>
+                    <button
+                        className={S.loginButton({ socialType: 'kakao' })}
+                        onClick={() => handleLoginButtonClick('kakao')}
+                    >
+                        <KakaoLogoIcon />
+                        카카오 로그인
+                    </button>
+
+                    <button
+                        className={S.loginButton({ socialType: 'apple' })}
+                        onClick={() => handleLoginButtonClick('apple')}
+                    >
+                        <AppleLogoIcon />
+                        Apple 로그인
+                    </button>
+                </div>
+            </section>
         </div>
     );
 };
