bcf6e191a0dea75962dcaa8f830180b78e083c14
Baik Gwangin <gwangin1999@naver.com>
2024-02-04 01:52:19 +0900

    ✨ 로그인 페이지 라우터 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import MainPage from "@/pages/main";
+import LoginPage from "@/pages/auth";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        // FIXME : 공통 fallback 컴포넌트 적용 필요
         errorElement: <div>에러</div>,
         element: <MainPage />,
-        children: [
-            {
-                path: "auth",
-                element: <MainPage />
-            }
-        ]
     },
+    {
+        path: "auth",
+        element: <LoginPage />
+    }
 ])
