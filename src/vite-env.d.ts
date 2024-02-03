11c52ef04c7ba9cf760c528712d82de3dc6fd986
Baik Gwangin <gwangin1999@naver.com>
2024-02-04 01:39:38 +0900

    🐛 일부 환경변수에 대한 타입 정의

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,9 @@ interface ImportMetaEnv {
     readonly VITE_KAKAO_REDIRECTION_URL: string;
     readonly VITE_APPLE_CLIENT_ID: string;
     readonly VITE_APPLE_REDIRECT_URL: string;
+    readonly VITE_TMAP_APP_KEY: string;
+    readonly VITE_SERVER_URL: string;
+
 }
 
 interface ImportMeta {
