5d1afb8d1eb0e5e960a662584e7c23a862198a23
Baik Gwangin <gwangin1999@naver.com>
2024-02-12 03:32:43 +0900

    🔥 더 이상 사용하지 않는 loader 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import LoginPage from '@/pages/login';
 import MainPage from '@/pages/main';
-import { MainPageLoader } from '@/pages/main/MainPage.loader';
 
 export const queryClient = new QueryClient({
     defaultOptions: {
@@ -18,7 +17,6 @@ export const router = createBrowserRouter([
         path: '/',
         errorElement: <div>에러</div>,
         element: <MainPage />,
-        loader: MainPageLoader(queryClient),
     },
     {
         path: 'login',
