899ba518a4e5f730c3d19307ff557bf6cbbdb13d
Baik Gwangin <gwangin1999@naver.com>
2024-02-12 03:47:26 +0900

    ✨ mediaQuery foundation 추가

--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -1,8 +1,9 @@
 import { createGlobalTheme } from '@vanilla-extract/css';
 
-import { FONT, COLOR } from './foundation';
+import { FONT, COLOR, MEDIA_QUERY } from './foundation';
 
 export const theme = createGlobalTheme(':root', {
     font: FONT,
     color: COLOR,
+    mediaQuery: MEDIA_QUERY
 });
