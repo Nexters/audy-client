114240bd2365071ef954b71ee24df76f826cd738
Baik Gwangin <gwangin1999@naver.com>
2024-02-12 03:33:07 +0900

    ✨ 유저 정보를 가져오는 useGetUserInformation 훅 제작

---
+++ src/query-hooks/user/query.ts
@@ -0,0 +1,32 @@
+import {
+    type UseSuspenseQueryOptions,
+    useSuspenseQuery,
+} from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
+
+import { UserRepository } from '@/apis/user';
+import { UserResponseType } from '@/apis/user/type';
+
+import { QUERY_KEY } from './key';
+
+export const useGetUserInformation = ({
+    userId,
+    options,
+}: {
+    userId?: string;
+    options: UseSuspenseQueryOptions<
+        UserResponseType['getInformation'],
+        AxiosError,
+        UserResponseType['getInformation']
+    >;
+}) => {
+    return useSuspenseQuery<
+        UserResponseType['getInformation'],
+        AxiosError,
+        UserResponseType['getInformation']
+    >({
+        ...options,
+        queryFn: () => UserRepository.getInformationAsync(userId),
+        queryKey: QUERY_KEY.info(userId),
+    });
+};
