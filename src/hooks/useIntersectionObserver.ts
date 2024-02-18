0b0f5a2d37f03b86af5a8f408ffb463c5f1ae557
Gwangin Baik <gwangin1999@naver.com>
2024-02-18 21:26:45 +0900

    [AUD-66] useIntersectionObserver Hook 제작 (#38)
    * ✨ useIntersectionObserver 훅 제작
    
    * 🐛 cleanup 함수 추가 및 구조 일부 개선

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from 'react';
+
+interface PropsType extends IntersectionObserverInit {
+    onIntersect?: IntersectionObserverCallback;
+}
+
+// targetRef 가 사용자가 명시한 root 와 교차하는지를 검증하는 Hook useIntersectionObserver
+export const useIntersectionObserver = ({
+    root,
+    rootMargin,
+    threshold,
+    onIntersect,
+}: PropsType) => {
+    const targetRef = useRef<HTMLElement | null>(null);
+
+    useEffect(() => {
+        if (!targetRef.current) return;
+
+        const observer = new IntersectionObserver(
+            (entries, observer) => onIntersect?.(entries, observer),
+            { root, rootMargin, threshold },
+        );
+
+        observer.observe(targetRef.current);
+
+        return () => observer.disconnect();
+
+    }, [root, rootMargin, threshold, onIntersect]);
+
+    return { targetRef }
+};
