304cd81a30f224de1c959b4b295d921558e8a64b
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-21 02:02:42 +0900

    💄 marker에 숨김처리되었을 때의 스타일 추가

--- src/features/map/marker/Marker.tsx
+++ src/features/map/marker/Marker.tsx
@@ -1,16 +1,23 @@
-import MarkerImage from '@/assets/icons/marker.svg?react';
+import MarkerBlackImage from '@/assets/images/markerBlack.svg?react';
+import MarkerWhiteImage from '@/assets/images/markerWhite.svg?react';
 
 import * as S from './Marker.css';
 
 interface PropsType {
     order: number;
+    isHidden: boolean;
 }
 
-const Marker = ({ order }: PropsType) => {
+const Marker = ({ order, isHidden }: PropsType) => {
     return (
         <div>
-            <MarkerImage className={S.marker} />
-            <p className={S.numberWrapper}>{order}</p>
+            {isHidden ? (
+                <MarkerWhiteImage className={S.marker} />
+            ) : (
+                <MarkerBlackImage className={S.marker} />
+            )}
+
+            <p className={S.numberWrapper({ isHidden })}>{order}</p>
         </div>
     );
 };
