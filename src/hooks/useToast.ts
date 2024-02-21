a64ad8fdd3430887e3fce2a911142365bf660082
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-21 17:47:40 +0900

    💡useToast에 대한 설명 추가

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -2,6 +2,11 @@ import { useContext } from 'react';
 
 import { ToastContext } from '@/utils/toast/ToastProvider';
 
+/**
+ * Toast를 사용할 수 있는 Hook useToast
+ * setToast에 토스트 메시지를 전달하여 토스트를 띄울 수 있음
+ */
+
 export const useToast = () => {
     const { toastMessage, setToastMessage } = useContext(ToastContext);
 
