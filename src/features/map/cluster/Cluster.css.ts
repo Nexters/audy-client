7c1dff5b4a1fc3e3f9e2944255b00c6ab54c3eba
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-24 01:11:32 +0900

    ✨ Cluster 컴포넌트 구현
    - 스타일 미적용

---
+++ src/features/map/cluster/Cluster.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+import { COLOR } from '@/styles/foundation';
+
+export const layout = style({
+    backgroundColor: COLOR.MonoWhite,
+    borderRadius: '6px',
+    padding: '5px',
+});
