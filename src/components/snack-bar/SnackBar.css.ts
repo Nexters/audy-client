1ae9f82341787128a0a1e3536f9ba316d7258d6e
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-25 03:21:48 +0900

    ♻️ snackBar zIndex foundation 레벨에 정의

--- src/components/snack-bar/SnackBar.css.ts
+++ src/components/snack-bar/SnackBar.css.ts
@@ -9,7 +9,7 @@ const fadeOutAnimation = keyframes({
 });
 
 export const layout = style([
-    sprinkles({ typography: 'Bold14' }),
+    sprinkles({ typography: 'Bold14', zIndex: 'snackBar' }),
     {
         position: 'absolute',
         border: `1px solid ${COLOR.Gray200}`,
@@ -17,7 +17,6 @@ export const layout = style([
         borderRadius: '8px',
         padding: '14px 20px',
         color: COLOR.Gray800,
-        zIndex: 1000,
 
         animation: `${fadeOutAnimation} 3s ease-in-out`,
         animationDelay: '5s',
