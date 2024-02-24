e80a65862b590e4f19b35404b86773f9cefc8928
RookieAND <gwangin1999@naver.com>
2024-02-24 23:16:06 +0900

    ♻️ StompJS 기반으로 useSocket 훅 마이그레이션

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,57 +1,122 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
-import { Socket, io } from 'socket.io-client';
+import { Client as StompClient } from '@stomp/stompjs';
 
-const serverUrl = 'wss://api.audy-gakka.com/course';
+import type { CourseSocketSubType } from '@/apis/course/type';
+import { useTmap } from '@/hooks/useTmap';
+import { PinType } from '@/types';
 
-export const useSocket = () => {
-    const [socket, setSocket] = useState<Socket | null>(null);
-    const [receivedData, setReceivedData] = useState<string>('');
+export const useSocket = (courseId: number) => {
+    const stompClient = useRef<StompClient | null>(null);
+    const { tmapModuleRef } = useTmap();
 
-    useEffect(() => {
-        const newSocket = io(serverUrl);
-        setSocket(newSocket);
-
-        return () => {
-            newSocket.disconnect();
-        };
-    }, []);
-
-    useEffect(() => {
-        if (!socket) return;
-
-        const handleSocketData = (data: string) => {
-            setReceivedData(data);
-        };
-
-        socket.on('data', handleSocketData);
-
-        return () => {
-            socket.off('data', handleSocketData);
-        };
-    }, [socket]);
-
-    useEffect(() => {
-        const handleDisconnect = () => {
-            console.log('연결이 종료되었습니다.');
-        };
-
-        if (socket) {
-            socket.on('disconnect', handleDisconnect);
-        }
-
-        return () => {
-            if (socket) {
-                socket.off('disconnect', handleDisconnect);
-            }
-        };
-    }, [socket]);
-
-    const sendData = (data: string) => {
-        if (socket && socket.connected) {
-            socket.emit('sendData', data);
-        }
+    const modifyPinName = ({
+        pinId,
+        pinName,
+    }: {
+        pinId: string;
+        pinName: string;
+    }) => {
+        stompClient.current?.publish({
+            destination: `/pub/${courseId}/pin/modification`,
+            body: JSON.stringify({
+                pinId,
+                pinName,
+            }),
+        });
     };
 
-    return { sendData, receivedData };
+    const addPin = ({
+        courseId,
+        pinId,
+        pinName,
+        originName,
+        latitude,
+        longitude,
+        sequence,
+    }: PinType & { courseId: number }) => {
+        stompClient.current?.publish({
+            destination: `/pub/${courseId}/pin/addition`,
+            body: JSON.stringify({
+                courseId,
+                pinId,
+                pinName,
+                originName,
+                latitude,
+                longitude,
+                sequence,
+            }),
+        });
+    };
+
+    const removePin = (pinId: string) => {
+        stompClient.current?.publish({
+            destination: `/pub/${courseId}/pin/removal`,
+            body: JSON.stringify({
+                courseId,
+                pinId,
+            }),
+        });
+    };
+
+    useEffect(() => {
+        const stomp = new StompClient({
+            brokerURL: 'wss://api.audy-gakka.com/course',
+            onConnect: () => {
+                console.log('Connected to the broker');
+                stomp.subscribe(`/sub/${courseId}/pin/addition`, (message) => {
+                    if (!tmapModuleRef.current) return;
+                    const newMarker: CourseSocketSubType['addition'] =
+                        JSON.parse(message.body);
+                    tmapModuleRef.current.createMarker({
+                        id: newMarker.pinId,
+                        name: newMarker.pinName,
+                        originName: newMarker.originName,
+                        address: newMarker.address,
+                        lat: String(newMarker.latitude),
+                        lng: String(newMarker.longitude),
+                    });
+                });
+                stomp.subscribe(
+                    `/sub/${courseId}/pin/modification`,
+                    (message) => {
+                        if (!tmapModuleRef.current) return;
+                        const {
+                            pinId,
+                            pinName,
+                        }: CourseSocketSubType['modification'] = JSON.parse(
+                            message.body,
+                        );
+                        console.log(pinId, pinName);
+                    },
+                );
+                stomp.subscribe(`/sub/${courseId}/pin/removal`, (message) => {
+                    if (!tmapModuleRef.current) return;
+                    const { pinId }: CourseSocketSubType['removal'] =
+                        JSON.parse(message.body);
+                    tmapModuleRef.current.removeMarker(pinId);
+                });
+            },
+            onDisconnect: () => {
+                console.log('Disconnected from the broker');
+                stomp.deactivate();
+            },
+            onStompError: (frame) => {
+                console.log('Broker reported error:', frame);
+            },
+        });
+
+        stomp.activate();
+        stompClient.current = stomp;
+
+        return () => {
+            stompClient.current?.deactivate();
+        };
+    }, [courseId, tmapModuleRef]);
+
+    return {
+        modifyPinName,
+        addPin,
+        removePin,
+    };
 };
