6a49c9cb4d2bc617449deda87031864af0584949
RookieAND <gwangin1999@naver.com>
2024-02-25 06:53:19 +0900

    🐛 마커 초기화 시 무한 렌더링 이슈 임시 수정

--- src/features/path/path-view/PathView.tsx
+++ src/features/path/path-view/PathView.tsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from 'react';
 import { Reorder } from 'framer-motion';
 import { useParams } from 'react-router-dom';
 
+import { SearchContextValue } from '@/features/search/search-context';
 import { useDebounce } from '@/hooks/useDebounce';
 import { useEventListeners } from '@/hooks/useEventListeners';
 import { useTmap } from '@/hooks/useTmap';
@@ -13,7 +14,6 @@ import PathItem from '../path-item';
 
 import * as S from './PathView.css';
 import PathViewContextProvider from './PathViewContextProvider';
-import { SearchContextValue } from '@/features/search/search-context';
 
 const REORDER_DELAY = 330;
 
@@ -36,6 +36,24 @@ const PathView = () => {
     useEventListeners('marker:remove', (event) => {
         setMarkers(markers.filter((marker) => marker.id !== event.detail));
     });
+
+    useEffect(() => {
+        if (!tmapModuleRef.current) return;
+        const initMarkerList: MarkerType[] = pinList
+            .map(({ pinName, pinId, address, latitude, longitude }) =>
+                tmapModuleRef.current?.createMarker({
+                    name: pinName,
+                    originName: pinName,
+                    address,
+                    id: pinId,
+                    lat: String(latitude),
+                    lng: String(longitude),
+                }),
+            )
+            .filter((marker): marker is MarkerType => !!marker);
+        setMarkers(initMarkerList);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const debouncedModifyMarker = debounce((newOrder: MarkerType[]) => {
         if (!tmapModuleRef.current) return;
