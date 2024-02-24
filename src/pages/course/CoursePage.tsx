01b7a6db4da2a952d5282d803e627145b32042ec
RookieAND <gwangin1999@naver.com>
2024-02-25 05:46:26 +0900

    ✨ CourseNameInput 컴포넌트 적용

--- src/pages/course/CoursePage.tsx
+++ src/pages/course/CoursePage.tsx
@@ -3,10 +3,9 @@ import { useState } from 'react';
 import clsx from 'clsx';
 import { useParams } from 'react-router-dom';
 
-import LeftArrowIcon from '@/assets/icons/leftArrow.svg?react';
-import ModifyFilledIcon from '@/assets/icons/modifyFilled.svg?react';
 import GlobalNavigationBar from '@/components/global-navigation-bar';
 import SidePanel from '@/components/side-panel';
+import CourseNameInput from '@/features/course/course-name-input';
 import FloatMenu from '@/features/map/float-menu';
 import PathView from '@/features/path/path-view';
 import SearchBar from '@/features/search/search-bar';
@@ -22,23 +21,11 @@ function CoursePage() {
     const { courseId } = useParams();
 
     const {
-        data: { courseName, pinList = [] },
+        data: { pinList = [] },
     } = useGetCourseDetail({ courseId: Number(courseId) });
 
     const [isSearchMode, setIsSearchMode] = useState(false);
     const [searchResults, setSearchResults] = useState<SearchResultType[]>([]);
-
-    const [isCourseNameEditing, setIsCourseNameEditing] = useState(false);
-    const [editedCourseName, setEditedCourseName] = useState(courseName);
-
-    const handleCourseNameChange = ({
-        target,
-    }: React.ChangeEvent<HTMLInputElement>) =>
-        setEditedCourseName(target.value);
-
-    const handleCourseNameSave = () => {
-        setIsCourseNameEditing(false);
-    };
 
     return (
         <>
@@ -46,45 +33,7 @@ function CoursePage() {
 
             <div className={S.wrapper}>
                 <SidePanel>
-                    <div className={S.header}>
-                        {isCourseNameEditing || (
-                            <LeftArrowIcon
-                                width={24}
-                                height={24}
-                                className={S.backArrowIcon}
-                            />
-                        )}
-
-                        {isCourseNameEditing ? (
-                            <>
-                                <input
-                                    type="text"
-                                    className={S.courseNameInput}
-                                    autoFocus
-                                    value={editedCourseName}
-                                    onChange={handleCourseNameChange}
-                                />
-                                <button
-                                    className={S.courseNameSaveButton}
-                                    onClick={handleCourseNameSave}
-                                >
-                                    저장
-                                </button>
-                            </>
-                        ) : (
-                            <>
-                                <p className={S.courseName}>
-                                    {editedCourseName}
-                                </p>
-                                <button
-                                    className={S.courseNameEditButton}
-                                    onClick={() => setIsCourseNameEditing(true)}
-                                >
-                                    <ModifyFilledIcon width={24} height={24} />
-                                </button>
-                            </>
-                        )}
-                    </div>
+                    <CourseNameInput />
 
                     <SearchBar
                         isSearchMode={isSearchMode}
