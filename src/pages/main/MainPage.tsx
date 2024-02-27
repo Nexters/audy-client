e2d80bea55af1ed6fc85a183eea574279744bc28
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-27 21:26:15 +0900

    ✨ 새로운 코스 만들기 기능 구현
    - 코스가 만들어지면 바로 해당 코스로 이동함

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import AddIcon from '@/assets/icons/add.svg?react';
 import AsyncBoundary from '@/components/async-boundary';
 import GlobalNavigationBar from '@/components/global-navigation-bar';
@@ -7,18 +9,31 @@ import SidePanel from '@/components/side-panel';
 import CourseFilteringTab from '@/features/course/course-filtering-tab/CourseFilteringTab';
 import CoursesContainer from '@/features/course/courses-container';
 import { useTmap } from '@/hooks/useTmap';
+import { usePostSaveCourse } from '@/query-hooks/course/mutation';
+import { useGetUserInformation } from '@/query-hooks/user/query';
 import { CourseTabType } from '@/types';
 
 import * as S from './MainPage.css';
 
 const MainPage = () => {
     const { mapContainerRef } = useTmap();
+    const navigate = useNavigate();
+
+    const { data: userInformation } = useGetUserInformation({});
+    const { mutateAsync: makeNewCourse } = usePostSaveCourse({
+        userId: userInformation.userId,
+    });
 
     const [selectedCourseTab, setSelectedCourseTab] =
         useState<CourseTabType>('allCourse');
 
     const handleCourseTabClick = (tab: CourseTabType) => {
         setSelectedCourseTab(tab);
+    };
+
+    const handleMakeNewCourse = async () => {
+        const response = await makeNewCourse('새로운 코스');
+        navigate(`/course/${response.data.courseId}`);
     };
 
     return (
@@ -32,7 +47,10 @@ const MainPage = () => {
                         handleCourseTabClick={handleCourseTabClick}
                     />
 
-                    <button className={S.addNewCourseButton}>
+                    <button
+                        className={S.addNewCourseButton}
+                        onClick={handleMakeNewCourse}
+                    >
                         <AddIcon fill="#000000" width={20} height={20} />
                         <p className={S.addNewCourseButtonText}>
                             새로운 코스 만들기
