6a5010ca7c036969bfc13504116689113f36e8b4
김가은 <80266418+KimGaeun0806@users.noreply.github.com>
2024-02-28 00:25:40 +0900

    [AUD-74] 토스트, 스낵바 스타일 변경 (#53)
    * 💄 토스트 스타일 수정
    
    * 🩹 토스트, 스낵바 표출 시간 변경
    
    * 💄 스낵바 스타일 변경
    
    - 닫기 버튼 추가
    
    * 💄 SnackBar에 닫기 아이콘 추가
    
    * 🩹 잘못 주석처리된 코드 수정
    
    * 💄 Toast 스타일 수정

--- src/components/snack-bar/SnackBar.css.ts
+++ src/components/snack-bar/SnackBar.css.ts
@@ -9,16 +9,49 @@ const fadeOutAnimation = keyframes({
 });
 
 export const layout = style([
-    sprinkles({ typography: 'Bold14', zIndex: 'snackBar' }),
+    sprinkles({ typography: 'SemiBold14', zIndex: 'snackBar' }),
     {
         position: 'absolute',
+        left: '50%',
+        top: '73px',
+        transform: 'translateX(-50%)',
         border: `1px solid ${COLOR.Gray200}`,
         backgroundColor: COLOR.MonoWhite,
         borderRadius: '8px',
-        padding: '14px 20px',
+        padding: '0 8px 0 20px',
         color: COLOR.Gray800,
+        width: '320px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
 
         animation: `${fadeOutAnimation} 3s ease-in-out`,
-        animationDelay: '5s',
+        animationDelay: '8s',
     },
 ]);
+
+export const buttonsContainer = style({
+    display: 'flex',
+    alignItems: 'center',
+});
+
+export const undoButton = style([
+    sprinkles({ typography: 'SemiBold14' }),
+    {
+        padding: '5px 8px',
+        color: COLOR.Gray500,
+        backgroundColor: 'rgba(0, 0, 0, 0.03)',
+        borderRadius: '4px',
+        transition: 'background-color 0.2s',
+
+        ':hover': {
+            backgroundColor: COLOR.Blue500,
+            color: COLOR.MonoWhite,
+        },
+    },
+]);
+
+export const closeButton = style({
+    color: COLOR.Gray400,
+    padding: '10px',
+});
