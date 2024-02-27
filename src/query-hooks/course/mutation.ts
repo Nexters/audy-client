001eb16302f5a19fe3134c1bc4b7544894dd2f5c
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-27 21:25:31 +0900

    ✨ usePostSaveCourse hook 구현

--- src/query-hooks/course/mutation.ts
+++ src/query-hooks/course/mutation.ts
@@ -1,8 +1,13 @@
-import { type UseMutationOptions, useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+    type UseMutationOptions,
+    useMutation,
+    useQueryClient,
+} from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
 
 import { CourseRepository } from '@/apis/course';
 import type { CourseRequestParamType } from '@/apis/course/type';
-import type { AxiosError } from 'axios';
+
 import { COURSE_QUERY_KEY } from './key';
 
 // 특정 코스를 삭제하는 Hook useDeleteCourse
@@ -10,11 +15,13 @@ export const useDeleteCourse = ({
     userId,
     courseId,
     ...options
-}: CourseRequestParamType['deleteCourse'] & UseMutationOptions<void, AxiosError>) => {
+}: CourseRequestParamType['deleteCourse'] &
+    UseMutationOptions<void, AxiosError>) => {
     const queryClient = useQueryClient();
     return useMutation({
         ...options,
-        mutationFn: () => CourseRepository.deleteCourseAsync({ userId, courseId }),
+        mutationFn: () =>
+            CourseRepository.deleteCourseAsync({ userId, courseId }),
         onSuccess: () => {
             queryClient.removeQueries({
                 queryKey: COURSE_QUERY_KEY.detail(courseId),
@@ -32,10 +39,32 @@ export const usePatchCourseName = ({
     const queryClient = useQueryClient();
     return useMutation({
         ...options,
-        mutationFn: (courseName: string) => CourseRepository.patchUpdateCourseAsync({ courseId, courseName }),
+        mutationFn: (courseName: string) =>
+            CourseRepository.patchUpdateCourseAsync({ courseId, courseName }),
         onSuccess: () => {
             queryClient.invalidateQueries({
                 queryKey: COURSE_QUERY_KEY.detail(courseId),
+            });
+        },
+        throwOnError: true,
+    });
+};
+
+// 새로운 코스를 만드는 Hook usePostSaveCourse
+export const usePostSaveCourse = ({
+    userId,
+    ...options
+}: {
+    userId: number;
+}) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        ...options,
+        mutationFn: (courseName: string) =>
+            CourseRepository.postSaveCourseAsync({ userId, courseName }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: COURSE_QUERY_KEY.list(),
             });
         },
         throwOnError: true,
