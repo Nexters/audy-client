2d47e22b34f55f63966fc094dcdc4c7f0f2a1e24
Baik Gwangin <gwangin1999@naver.com>
2024-02-27 23:48:10 +0900

    ✨ 로그아웃 모달 생성 및 기능 연동

--- src/components/global-navigation-bar/GlobalNavigationBar.tsx
+++ src/components/global-navigation-bar/GlobalNavigationBar.tsx
@@ -1,27 +1,47 @@
 import AudyLogoIcon from '@/assets/icons/audyLogo.svg?react';
 import SettingIcon from '@/assets/icons/setting.svg?react';
 import PopOver from '@/components/pop-over';
+import SignOutModal from '@/features/auth/sign-out-modal/SignOutModal';
+import { useDisclosure } from '@/hooks/useDisclosure';
+import { useModal } from '@/hooks/useModal';
 
 import * as S from './GlobalNavigationBar.css';
 
-const GlobalNavigationBar = () => (
-    <div className={S.wrapper}>
-        <AudyLogoIcon />
-        <p className={S.savingStatus}>수정된 코스 저장중...</p>
-        <PopOver>
-            <PopOver.Trigger>
-                <SettingIcon className={S.settingIcon} />
-            </PopOver.Trigger>
-            <PopOver.Content className={S.settingContent}>
-                <PopOver.Item className={S.logoutText} >
-                    <p>로그아웃</p>
-                </PopOver.Item>
-                <PopOver.Item className={S.withdrawText}>
-                    <p>회원탈퇴</p>
-                </PopOver.Item>
-            </PopOver.Content>
-        </PopOver>
-    </div>
-);
+const GlobalNavigationBar = () => {
+    const { value: isSignOutModalOpen, open: openSignOutModal } =
+        useDisclosure(false);
+
+    const { openModal } = useModal();
+
+    const handleSignOutButtonClick = () => {
+        openModal(<SignOutModal />);
+    };
+
+    return (
+        <>
+            <div className={S.wrapper}>
+                <AudyLogoIcon />
+                <p className={S.savingStatus}>수정된 코스 저장중...</p>
+                <PopOver>
+                    <PopOver.Trigger>
+                        <SettingIcon className={S.settingIcon} />
+                    </PopOver.Trigger>
+                    <PopOver.Content className={S.settingContent}>
+                        <PopOver.Item
+                            className={S.logoutText}
+                            onClick={handleSignOutButtonClick}
+                        >
+                            <p onClick={openSignOutModal}>로그아웃</p>
+                        </PopOver.Item>
+                        <PopOver.Item className={S.withdrawText}>
+                            <p>회원탈퇴</p>
+                        </PopOver.Item>
+                    </PopOver.Content>
+                </PopOver>
+            </div>
+            {isSignOutModalOpen && <SignOutModal />}
+        </>
+    );
+};
 
 export default GlobalNavigationBar;
