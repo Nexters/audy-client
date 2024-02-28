a65bcdc360b148650b4cb95a4b329a0fb5b974c7
KimGaeun <greencloud11@hanyang.ac.kr>
2024-02-29 02:12:18 +0900

    ✨ CourseTab에 코스명 수정 기능 추가

--- src/features/course/course-tab/ThreeDotButton.tsx
+++ src/features/course/course-tab/ThreeDotButton.tsx
@@ -3,21 +3,25 @@ import ThreeDotIcon from '@/assets/icons/threeDot.svg?react';
 import TrashCanIcon from '@/assets/icons/trashCan.svg?react';
 import PopOver from '@/components/pop-over';
 import { useModal } from '@/hooks/useModal';
-import { useGetUserInformation } from '@/query-hooks/user/query';
 
+import CourseNameEditModal from '../course-name-edit-modal';
 import CourseRemoveModal from '../course-remove-modal/CourseRemoveModal';
 
 import * as S from './ThreeDotButton.css';
 
 interface PropsType {
     courseId: number;
+    courseName: string;
 }
 
-const ThreeDotButton = ({ courseId }: PropsType) => {
+const ThreeDotButton = ({ courseId, courseName }: PropsType) => {
     const { openModal } = useModal();
 
-    // TODO : 모달 컴포넌트 개발 이후 수정 모달 추가 필요
-    const handleCourseEditIconClick = () => {};
+    const handleCourseEditIconClick = () => {
+        openModal(
+            <CourseNameEditModal courseId={courseId} courseName={courseName} />,
+        );
+    };
 
     const handleRemoveCourseIconClick = () => {
         openModal(<CourseRemoveModal courseId={courseId} />);
